a0869ece8cb436dd5ce372d4c8695e74f3090297
Sabri Chamtouri <149872304+sabricham@users.noreply.github.com>
2025-03-15 02:26:29 +0100

    utility for queue tasks integrated

--- main/main.c
+++ main/main.c
@@ -9,13 +9,25 @@
 /*--------------------------------------------------------------------------------*/
 
 #include <stdio.h>
+
 #include <esp_log.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+
+#include "MotorController.h"
+
+#include "gpioHandler.h"
+#include "queueHandler.h"
+#include "taskHandler.h"
 
 /*--------------------------------------------------------------------------------*/
 // Macros and Defines
 /*--------------------------------------------------------------------------------*/
 
-#define TAG "MAIN"
+#define TAG         "Main"
+QueueHandle_t mainQueue = NULL;
+queueMessage mainQueueMessage;
+int mainMessagesParams[MESSAGE_PARAMS_LENGTH];
 
 /*--------------------------------------------------------------------------------*/
 // Private variables
@@ -33,6 +45,37 @@
  */
 void app_main(void)
 {
-    ESP_LOGI(TAG, "Hello World");
+    // Create the MotorController task
+
+    
+    ESP_LOGW(TAG, "System startup procedure");
+    ESP_LOGW(TAG, "-------------------------------------------");
+    ESP_LOGW(TAG, "             Hexapod Robot                 ");
+    ESP_LOGW(TAG, "                 v1.0                      ");
+    ESP_LOGW(TAG, "               Prototype                   ");
+    ESP_LOGW(TAG, "-------------------------------------------");
+
+    ESP_LOGW(TAG, "Starting system tasks");
+    StartTask(MotorController, "MotorController", configMINIMAL_STACK_SIZE * 2, tskIDLE_PRIORITY + 2);
+    ESP_LOGW(TAG, "System tasks started successfully");
+    
+    ESP_LOGW(TAG, "Starting task");
+    CreateQueue(&mainQueue, &mainQueueMessage, 10, TAG);
+    ESP_LOGW(TAG, "Task started correctly");
+
+    while(1)
+    {
+        // Get messages from other tasks
+        if(xQueueReceive(mainQueue, &mainQueueMessage, 0))
+        {
+            ESP_LOGI(TAG, "Received message");
+        }
+
+        // Task logic
+        vTaskDelay(1);
+    }
+
+    ESP_LOGE(TAG, "This section should not be reached");
+    return;
 }
 /*End of main.c*/
---
+++ src/utility/timerHandler.h
@@ -0,0 +1,49 @@
+#ifndef _TIMER_H_
+#define _TIMER_H_
+
+//====================================================================================== 
+//   Includes 
+//======================================================================================
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/queue.h"
+#include "driver/gptimer.h"
+#include "esp_log.h"
+
+//====================================================================================== 
+//   Macros
+//======================================================================================
+
+//====================================================================================== 
+//   Public variables & defines
+//======================================================================================
+
+//====================================================================================== 
+//   Public functions & routines
+//======================================================================================
+
+void TimerCreate(gptimer_handle_t *timerHandle, 
+    int priority, 
+    uint64_t resolution, 
+    bool autoReload, 
+    gptimer_clock_source_t clockSource, 
+    gptimer_count_direction_t countDirection, 
+    uint64_t triggerValue, 
+    uint64_t startingValue, 
+    gptimer_alarm_cb_t timerCallback, 
+    void * args);
+void TimerEnable(gptimer_handle_t timerHandle);
+void TimerDisable(gptimer_handle_t timerHandle);
+void TimerReset(gptimer_handle_t timerHandle, uint64_t startingValue);
+void TimerSet(gptimer_handle_t timerHandle, uint64_t startingValue);
+uint64_t TimerGet(gptimer_handle_t timerHandle);
+void TimerStart(gptimer_handle_t timerHandle);
+void TimerStop(gptimer_handle_t timerHandle);
+
+#endif /* _TIMER_H_ */
